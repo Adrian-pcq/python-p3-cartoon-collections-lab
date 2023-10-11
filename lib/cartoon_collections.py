128b8f5ade21cd9458d4c3a1e437bcf9de8e7077
Adrian <adrianqueizan@gmail.com>
2023-10-11 18:05:00 -0400

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    i=1
+    for dwarve in dwarves:
+        print(f"{i}. {dwarve}")
+        i+=1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [word.title() + "!" for word in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    for word in list: 
+        if len(word)>4:
+            return True
+    return False 
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    for food in list:
+        if food == "cheddar":
+            return food
+    return None 
